b94ab06bb29550555b9d7cbf25cc15b455fb6c5a
bapah5 <pmigal1996@gmail.com>
2020-10-12 14:04:47 +0300

    add

--- telegram.py
+++ telegram.py
@@ -11,60 +11,58 @@ password_itilium = os.environ['PasswordItilium']
 auth_key = os.environ['AuthKey']
 
 
-@app.route('/getWebhookInfo',  methods=['GET'])
+@app.route('/getWebhookInfo', methods=['GET'])
 def getWebHookInfo():
     # это только на время теста, надо скрывать в продакшене
     ret = requests.post("https://api.telegram.org/bot" + auth_key + "/getWebhookInfo")
     return ret.text
 
-@app.route('/setWebHook',  methods=['GET'])
+
+@app.route('/setWebHook', methods=['GET'])
 def setWebHook():
     dict_data = dict()
-    address = request.url.replace("setWebHook",auth_key)
+    address = request.url.replace("setWebHook", auth_key)
     print(address)
-    dict_data.update( {"url": address} )
-    
-    print(dict_data)
-    
-    ret = requests.post("https://api.telegram.org/bot" + auth_key + "/setWebhook", 
-        data = json.dumps(dict_data).encode('utf-8'), 
-        headers = {"Content-Type" : "application/json"})
-    
-    
-    return ret.text
-    
+    dict_data.update({"url": address})
 
-@app.route('/removeWebHook',  methods=['GET'])
+    print(dict_data)
+
+    ret = requests.post("https://api.telegram.org/bot" + auth_key + "/setWebhook",
+                        data=json.dumps(dict_data).encode('utf-8'),
+                        headers={"Content-Type": "application/json"})
+
+    return ret.text
+
+
+@app.route('/removeWebHook', methods=['GET'])
 def removeWebHook():
     dict_data = dict()
-    dict_data.update( {"url": ""} )
-    
-    ret = requests.post("https://api.telegram.org/bot" + auth_key + "/setWebhook", 
-        data = json.dumps(dict_data).encode('utf-8'), 
-        headers = {"Content-Type" : "application/json"})
-    
+    dict_data.update({"url": ""})
+
+    ret = requests.post("https://api.telegram.org/bot" + auth_key + "/setWebhook",
+                        data=json.dumps(dict_data).encode('utf-8'),
+                        headers={"Content-Type": "application/json"})
+
     return ret.text
 
 
-@app.route('/' + auth_key,  methods=['POST'])
+@app.route('/' + auth_key, methods=['POST'])
 def IncomingConnectionPost():
     print("new message")
     if request.headers.get('content-type') == 'application/json':
         json_string = request.get_data().decode('utf-8')
-        try:
-            print(json_string)
-        except:
-            print(str(json_string))
-
-        print("UnJSONed: " + str(json.loads(json_string)))
 
         update = json.loads(json_string)
+
+        print("UnJSONed: " + str(update))
+        print("update_id: " + str(update["update_id"]))
         content = ''
         try:
-            requestItilium = requests.post(address_api_itilium, data=json.dumps(json_string), auth=(login_itilium, password_itilium))
+            request_itilium = requests.post(address_api_itilium, data=json_string,
+                                            auth=(login_itilium, password_itilium))
 
-            if requestItilium.status_code == 200 and requestItilium.ok != False:
-                content = requestItilium.content
+            if request_itilium.status_code == 200 and request_itilium.ok:
+                content = request_itilium.content
 
                 if content != 'Действие не найдено!':
                     content = json.loads(content)
@@ -80,12 +78,8 @@ def IncomingConnectionPost():
         }
 
         try:
-            requestTelegram = requests.post('https://api.telegram.org/bot' + auth_key + '/sendMessage', data=content)  # запрос на отправку сообщения
+            requestTelegram = requests.post('https://api.telegram.org/bot' + auth_key + '/sendMessage', data=content)
         except:
             print('Send message for user error')
 
-
     return Response(status=200)
-
-
-
